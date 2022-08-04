8a1098141721a3a576d5e9bc345f2fdc5c17fafc
ItsFelixRodriguez <ItsFelixRodriguez@gmail.com>
2022-08-04 18:05:58 +0900

    function now returns computer choice.

--- myscripts.js
+++ myscripts.js
@@ -1,13 +1,25 @@
 var choice = ["Rock", "Paper","Scissor"];
 
+console.log("Hello World");
 
-
-function rps() {
-    let randChoice = choice[Math.floor(Math.random() * Math.length)];
-    
+function getComputerChoice(min,max){
+   return Math.floor(Math.random() *(max-min*1)+min)
+}
+const rndChoice = getComputerChoice(1,4)
+console.log(rndChoice);
+if (rndChoice == 1) {
+    console.log ( "Rock");
+} else if (rndChoice == 2){
+    console.log ("Paper");
+} else if (rndChoice== 3) {
+    console.log("Scissors")
 }
 
-function getComputerChoice(){
-    rps();
-    return randChoice;
-}
+//function rps() {
+ //   randChoice = choice[Math.floor(Math.random() * Math.length)];
+   // return randChoice;
+//}
+//console.log(rps)
+
+//return choice[Math.floor(Math.random() * Math.length)];   
+console.log() 
