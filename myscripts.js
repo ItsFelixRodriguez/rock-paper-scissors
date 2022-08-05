c818ae4f22c15db10ff8c7c641ae25fb42c779cd
ItsFelixRodriguez <ItsFelixRodriguez@gmail.com>
2022-08-05 18:55:27 +0900

    Restarted because I got myself lost in the weeds

--- myscripts.js
+++ myscripts.js
@@ -1,53 +1,13 @@
-var choice = ["Rock", "Paper","Scissor"];
+let getComputerChoice = [{choice: 'Rock', value: 0 }, {choice: 'Paper', value: 1},{choice: 'Scissors', value: 2}];
+let playerScore = 0;
+let compScore = 0;
+let playerChoice;
 
-console.log("Play me in Rock Paper Scissors??");
-
-function getComputerChoice(min,max){
-   return Math.floor(Math.random() *(max-min*1)+min)
+function computerPlay () {
+    let result = getComputerChoice[Math.floor(Math.random() * getComputerChoice.length)];
+    return result;
 }
-const rndChoice = getComputerChoice(1,4)
-console.log(rndChoice);
-if (rndChoice == 1) {
-    console.log ( "Rock");
-} else if (rndChoice == 2){
-    console.log ("Paper");
-} else  {
-    console.log("Scissors")
+function playRound (playerSelection, computerSelection) {
+let playerSelectionL = prompt ("Choose Your Weapon");
+return playerSelectionL.toLocaleUpperCase;
 }
-function playRound(playerSelection, computerSelection) {
-  console.log ("Aye you win!");
-    
-}
-
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-
-
-//let playerSelection = prompt("Choose Your Weapon");
-//var playerSelectionUp = playerSelection.toUpperCase();
-
-//if (playerSelectionUp = 'ROCK' || rndChoice == 1) {
-  //console.log ( "You chose" + " "+ playerSelectionUp + " "+ "Your Opponent Chose" + " "+ "ROCK");
-  //console.log ( "A TIE, Go Again")
-
-//}else if (playerSelectionUp = 'PAPER') {
- //   console.log ( "hmmm" + " "+ playerSelectionUp + " "+ "interesting")
-//}
-//else if (playerSelectionUp = 'SCISSORS') {
-//    console.log ('bitch');
-//}
-
-
- //function playerSelection () {
-  //let play  prompt ("Please choose your weapon")
-//}
-
-//function rps() {
- //   randChoice = choice[Math.floor(Math.random() * Math.length)];
-   // return randChoice;
-//}
-//console.log(rps)
-
-//return choice[Math.floor(Math.random() * Math.length)];   
-console.log() 
