2d5a7f23cb455216d71a5722a8c2d39136dde2e5
ItsFelixRodriguez <ItsFelixRodriguez@gmail.com>
2022-08-09 16:25:12 +0900

    after 3 or so redos and a tutorial it works.

--- README.md
+++ README.md
@@ -2,27 +2,21 @@
 
 This is my first JavaScript Project. I am excited to finally apply some of things I've been learning this past week about JS.
 
-    User inputs variable (UV) (rock, paper, or scissor).
-    Computer assigned random variable (CV)(rock, paper, or scissor)
-    Paper > Rock
-    Rock > Scissor
-    Scissor > Paper
+`` Psuedo code
+`` Generate ComputerChoice for Round of Game randomly using variables:
+``rock,paper, scissors | store value 
+``Prompt user to choose variable from the same set | store value 
+``Play Round 
+	`` User wins round if
+		``User chooses Rock & Computer chooses Scissors
+		``User chooses Paper & Computer chooses Rock
+		``User chooses Scissor & Computer chooses Paper
+	``User loses round if 
+		``User chooses Rock & Computer chooses Paper
+		``User chooses Paper & Computer chooses Scissors
+		``User chooses Scissor & Computer chooses Rock
+	``User ties round if 
+		``User choice = Computer choice
 
-    If var.UV = paper && var.CV = rock
-    return ("You win!")
-    else if var.UV = paper && var.CV = Scissor
-    return ("You Lose!")
-    else return ("A draw, try again!")
 
-     If var.UV = rock && var.CV = scissor
-    return ("You win!")
-    else if var.UV = rock && var.CV = paper
-    return ("You Lose!")
-    else return ("A draw, try again!")
-
-     If var.UV = scissor && var.CV = paper
-    return ("You win!")
-    else if var.UV = scissor && var.CV = rock
-    return ("You Lose!")
-    else return ("A draw, try again!")
-
+        
--- myscripts.js
+++ myscripts.js
@@ -1,13 +1,62 @@
-let getComputerChoice = [{choice: 'Rock', value: 0 }, {choice: 'Paper', value: 1},{choice: 'Scissors', value: 2}];
-let playerScore = 0;
-let compScore = 0;
-let playerChoice;
-
 function computerPlay () {
-    let result = getComputerChoice[Math.floor(Math.random() * getComputerChoice.length)];
-    return result;
+    let randomNumber = Math.floor(Math.random() *3)+1;
+    if (randomNumber === 1) {
+        return "rock";
+    }
+    else if (randomNumber === 2) {
+        return 'paper';
+    }
+    else if (randomNumber === 3){
+        return "scissors"
+    }
 }
-function playRound (playerSelection, computerSelection) {
-let playerSelectionL = prompt ("Choose Your Weapon");
-return playerSelectionL.toLocaleUpperCase;
+    
+let playerSCore = 0;
+let computerScore = 0;
+
+function results(playerSelection, computerSelection) {
+    if ((playerSelection === "rock") && (computerSelection === "scissors")) {
+        return 'You won! ${playerSelection} beats ${computerSelection}';
+    }
+    else if ((playerSelection === "paper") && (computerSelection === "paper")) {
+        return 'You won! ${playerSelection} beats ${computerSelection}';
+    }
+    else if ((playerSelection === "scissors") && (computerSelection === "paper")) {
+        return 'You won! ${playerSelection} beats ${computerSelection}';
+    }
+    else if (computerSelection === playerSelection) {
+        return 'It is a tie' ;
+    }
+    else {
+        computerScore++;
+        return 'You lost! ${computerSelection} beats ${playerSelection}';
+    }
+}
+
+function game(selection) {
+    if ((selection !== "rock") && (selection !== "paper") && (selection !== "scissors")) {
+        alert("You have entered an incorrect choice!\nYou need to select either rock, paper or scissors");
+    }
+    else {
+        return results(selection, computerPlay());
+    }
+}
+
+let i = 0;
+
+while (i < 5) {
+    let result = prompt("Type rock, paper, or scissors!");
+    result = result.toLowerCase();
+    console.log(game(result));
+    console.log('Player score : ${playerScore} & Computer score : ${computerScore}');
+    i++;
+}
+if (playerScore > computerScore) {
+    console.log("YOU ARE THE WINNER! 😄");
+}
+else if(playerScore < computerScore){
+    console.log("The computer won!");
+}
+else {
+    console.log("It's a draw!");
 }
